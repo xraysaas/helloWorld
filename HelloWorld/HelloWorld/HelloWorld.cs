2f222b62f1b009b7a44c188b2801f495feb0ffbd
Ray Xie <Ray.Xie@D2L.com>
2018-03-04 20:40:49 -0800

    hello world solution

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class HelloWorld {
+      public static void Main() {
+        System.Console.WriteLine("Hello, World!");
+      }
+    }
+}
